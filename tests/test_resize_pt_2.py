e84fb3f68d91f9af1d6edbb191b0f7f8ef2baad6
Shuo Han <shan50@jhu.edu>
2022-04-09 12:02:25 -0400

    Test resize

--- tests/test_resize_pt_2.py
+++ tests/test_resize_pt_2.py
@@ -11,11 +11,7 @@ def test_resize_cpu():
     coords = None
     for i in range(10000):
         print(i, a.device)
-        if coords is None:
-            b, coords = resize(a, (4, 1), order=3, return_coords=True)
-        else:
-            b = F.grid_sample(a, coords, mode='bicubic', align_corners=True,
-                              padding_mode='border')
+        b, coords = resize(a, (4, 1), order=3, return_coords=True)
 
 if __name__ == '__main__':
     test_resize_cpu()
